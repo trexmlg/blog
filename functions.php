7077d0a53a07c25cab04ee87352097c748dd7830
Skolnieks <skolnieks.pjvt@pjvt.local>
2025-01-06 09:49:51 +0200

    blog

--- functions.php
+++ functions.php
@@ -1,15 +1,10 @@
 <?php
 
-
-
-
-
-
 function dd($data){
     echo "<pre>";
     var_dump($data);
     echo "</pre>";
     die();
-     }
+}
 
 
--- index.php
+++ index.php
@@ -3,14 +3,22 @@
 require "functions.php";
 require "Database.php";
 
-echo "hi there";
+echo "<h1>hi there<h1/>";
 
 $config = require("confib.php");
 
 $db = new Database($config["database"]);
-$posts = $db->query("SELECT * FROM posts");
-$comments = $db->query("SELECT * FROM comments")->fetchALL(PDO::FETCH_ASSOC);
-// $user = $db->query(); //
+$posts = $db->query("SELECT * FROM post")->fetchAll();
+
+if (isset($_GET["search_query"]) && $_GET["search_query"] != "") {
+    $posts = $db->query("SELECT * FROM post WHERE content LIKE " . $_GET ["search_query"] )->fetchAll();
+};
+
+echo "<form >";
+echo "<input name='search_query' />";
+
+echo "<button>Meklēt<button/>";
+echo "<form/>";
 
 echo "<ul>";
 foreach ($posts as $post){
